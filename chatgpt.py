5f2a4ae4fef8cb670e945396d5c878a99c916952
HaXAlvin <qazqwe0922@gmail.com>
2023-04-26 00:57:57 +0800

    fix format validate and imporve backoff

--- chatgpt.py
+++ chatgpt.py
@@ -2,43 +2,58 @@ import json
 from schema import Schema, And, Use
 
 import os
-import backoff
 import bert_score
 import openai
-from openai.error import RateLimitError
+from openai.error import RateLimitError, APIError, ServiceUnavailableError
 from tqdm import tqdm
 from transformers import logging
+import time
+from multiprocessing import Pool, TimeoutError
+from functools import wraps
 
 logging.set_verbosity_error()
 openai.api_key = "YOUR_API_KEY"
 MODEL = "gpt-3.5-turbo"
 # MODEL = "gpt-4"
 BATCH = 2
-SUBMIT = 1
+SUBMIT = 0
 MAX_SNIPPET_LEN = 250
+TEMPERATURE = 0.7
 # dataset_path = "/Users/alvin/Projects/BioASQ11-Task b/data/BioASQ-training11b/training11b.json"
 dataset_path = "/Users/alvin/Projects/BioASQ11-Task b/data/BioASQ-task11bPhaseB-testset2.txt"
 
+with open(dataset_path, "r", encoding="utf-8") as f:
+    QUESTIONS = json.load(f)["questions"]
 
 SCHEMA = {
     "yesno": Schema({
-        "exact_answer": And(Use(str), Use(str.lower), lambda s: s in ('yes', 'no')),
-        "ideal_answer": And(Use(str), lambda s: s.strip() != ""),
-        "id": str
+        "exact_answer": And(
+            str,
+            Use(str.lower),  # lowercase the YES/NO
+            lambda s: s in ('yes', 'no')
+        ),
+        "ideal_answer": And(Use(str), lambda s: s.strip() != "", lambda s: len(s.split()) <= 200)
     }),
     "list": Schema({
-        "exact_answer": And(list, lambda l: 100 >= len(l) > 0, lambda l: all(100 >= len(i) > 0 for i in l)),
-        "ideal_answer": And(Use(str), lambda s: s.strip() != ""),
-        "id": str
+        "exact_answer": And(
+            list,
+            lambda l: 100 >= len(l) > 0,  # no more than 100 entries
+            lambda l: all((100 >= len(i) > 0) and isinstance(i, str) for i in l),  # no more than 100 characters each
+            Use(lambda l: [[i] for i in l])  # convert to list of list
+        ),
+        "ideal_answer": And(Use(str), lambda s: s.strip() != "", lambda s: len(s.split()) <= 200)
     }),
     "summary": Schema({
-        "ideal_answer": And(Use(str), lambda s: s.strip() != ""),
-        "id": str
+        "ideal_answer": And(Use(str), lambda s: s.strip() != "", lambda s: len(s.split()) <= 200)
     }),
     "factoid": Schema({
-        "exact_answer": And(list, Use(lambda x: x[:5]), lambda l: 5 >= len(l) > 0),
-        "ideal_answer": And(Use(str), lambda s: s.strip() != ""),
-        "id": str
+        "exact_answer": And(
+            list,
+            # Use(lambda x: x[:5]),  # TODO: select top 5 entries
+            lambda l: 5 >= len(l) > 0,  # no more than 5 entries
+            Use(lambda l: [[i] for i in l])  # convert to list of list
+        ),
+        "ideal_answer": And(Use(str), lambda s: s.strip() != "", lambda s: len(s.split()) <= 200)
     })
 }
 
@@ -58,41 +73,61 @@ def average(scores: list):
     return sum(scores) / len(scores)
 
 
-@backoff.on_exception(backoff.expo, RateLimitError, max_time=60, max_tries=5)
+def gpt_api_retry(func):
+    @wraps(func)
+    def warp_func(**kwargs):
+        log = ""
+        for i in range(5):
+            try:
+                return func(**kwargs)
+            except (RateLimitError, APIError, ServiceUnavailableError, TimeoutError) as e:
+                message = f"Retry: {i+1} times, error: {e}\n\n"
+                print(message)
+                log += message
+                time.sleep(3)
+        raise TimeoutError(f"Failed to get response from OpenAI API after 5 retries.\n\n{log}")
+    return warp_func
+
+
+@gpt_api_retry
 def completions_with_backoff(**kwargs):
-    return openai.ChatCompletion.create(**kwargs)
+    with Pool(processes=1) as pool:
+        process = pool.apply_async(openai.ChatCompletion.create, kwds=kwargs)
+        return process.get(timeout=60)
 
 
-with open(dataset_path, "r", encoding="utf-8") as f:
-    QUESTIONS = json.load(f)["questions"]
+def get_question_answer(q):
+    resp = None
+    try:  # get result from gpt request
+        messages = [make_message("assistant", sni["text"][:MAX_SNIPPET_LEN]) for sni in q["snippets"]]
+        # TODO: select top n snippets?
+        messages += [make_message("user", PROMPT[q["type"]]), make_message("user", q["body"])]
+        completion = completions_with_backoff(model=MODEL, messages=messages, temperature=TEMPERATURE)
+        resp = completion.choices[0].message.content.strip(".。\"'")
+        result = json.loads(resp) if q["type"] != "summary" else {"ideal_answer": resp}
+        result = SCHEMA[q["type"]].validate(result)
+        return result
+    except Exception as e:
+        print("====Error, check error.txt====")
+        with open("error.txt", "w", encoding="utf-8") as f:
+            f.write(f"{str(e)}\nid:{q['id']}\ntype:{q['type']}\nresp:{resp}\n")
+        raise e
 
 
 def request_gpt(specific_q=None):
     loop = tqdm(QUESTIONS) if specific_q is None else [specific_q]
-    try:
-        for i, q in enumerate(loop):
-            # print(q['id'])
-            path = f"gpt_result/11b_batch_{BATCH}_submit_{SUBMIT}/{q['id']}.json"
-            if not os.path.exists(path):
-                messages = [make_message("assistant", sni["text"][:MAX_SNIPPET_LEN]) for sni in q["snippets"]]
-                messages = messages[:5] + [make_message("user", PROMPT[q["type"]]), make_message("user", q["body"])]
-                # print(messages)
-                completion = completions_with_backoff(model=MODEL, messages=messages, temperature=0.7)
-                resp = completion.choices[0].message.content.strip(".。\"'")
-                resp_text = json.loads(resp) if resp[0] == "{" else {"ideal_answer": resp}
-            else:
-                with open(path, "r", encoding="utf-8") as f:
-                    resp_text = json.load(f)
-            result = {"id": q["id"], **resp_text}
-            result = SCHEMA[q["type"]].validate(result)
-            with open(path, "w", encoding="utf-8") as f:
-                json.dump(result, f, ensure_ascii=False, indent=4)
-    except Exception as e:
-        print(e)
-        print(q["id"], q["type"])
-        print(resp_text)
-        with open("error.txt", "w", encoding="utf-8") as f:
-            f.write(f"{str(e)}\n\nProcess:{i}")
+    folder = f"gpt_result/11b_batch_{BATCH}_submit_{SUBMIT}"
+    if not os.path.isdir(folder):
+        os.mkdir(folder, mode=0o755)
+    for question in loop:
+        file_path = f"{folder}/{question['id']}.json"
+        if os.path.exists(file_path):  # skip if already exists
+            continue
+        result = get_question_answer(question)
+        # add id and save file
+        result = {"id": question["id"], **result}
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
 
 
 # def extract_answer(question, have_answer=False):
@@ -184,21 +219,32 @@ def calc_score():
 
 def merge_result_and_question():
     submit_file = {"questions": []}
+    folder = f"gpt_result/11b_batch_{BATCH}_submit_{SUBMIT}"
     for q in QUESTIONS:
-        with open(f"gpt_result/11b_batch_{BATCH}_submit_{SUBMIT}/{q['id']}.json", "r", encoding="utf-8") as f:
+        with open(f"{folder}/{q['id']}.json", "r", encoding="utf-8") as f:
             result = json.load(f)
         merge_q = {**q, **result}
         submit_file["questions"].append(merge_q)
-    submit_file_path = f"gpt_result/11b_batch_{BATCH}_submit_{SUBMIT}/submit.json"
+    submit_file_path = f"{folder}/submit.json"
     if os.path.exists(submit_file_path):
-        input(f"overwriting {submit_file_path}. Press enter to continue.")
+        input(f"Overwriting {submit_file_path}. Press enter to continue.....")
     with open(submit_file_path, "w") as f:
         json.dump(submit_file, f, ensure_ascii=False, indent=4)
 
 
-# calc_score()
-# request_gpt()
-merge_result_and_question()
+def validate():
+    request_gpt()
+    calc_score()
+
+
+def test():
+    request_gpt()
+    merge_result_and_question()
+
+
+if __name__ == "__main__":
+    test()
+
 
 # 放在同個dict
 # exact_answer ideal_answer分開
